ff7f777d8c22b15cd6edb24f72a4ec853707502b
MMDeveloper <MMDeveloper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-25 05:18:54 +0000

    Updated tracking code. Allows you to upload images with new VC code. Allows you to use AJAX.
    git-svn-id: https://plugins.svn.wordpress.org/a-forms/tags/1.5.1@778116 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/controllers/a_form_fields_controller.php
@@ -0,0 +1,57 @@
+<?php
+final class AdminAFormFieldsController {
+	public static function updateAction() {
+		//if (AFormFieldsValidation::is_valid()) {
+      $field_valid = true;
+      $fields = $_POST["FID"];
+      $index = 0;
+      
+      foreach ($fields as $field) {
+        if ($_POST["FID"][$index] != "") {
+          $test_valid = tom_update_record_by_id("a_form_fields", 
+            array(
+              "section_id" => $_POST["section_id"][$index],
+              "field_label" => $_POST["field_label"][$index],
+              "field_type" => $_POST["field_type"][$index],
+              "field_order" => $_POST["field_order"][$index],
+              "validation" => $_POST["validation_0"][$index]." ".$_POST["validation_1"][$index],
+              "value_options" => $_POST["value_options"][$index],
+              "file_ext_allowed" => tom_get_query_string_value("file_ext_allowed", $index)
+            ),
+            "FID",
+            $_POST["FID"][$index]
+          );
+          
+          if (!$test_valid) {
+            echo "FAIL";
+            // If one of the records fails, then fail the lot.
+            $field_valid = false;
+          }
+        }
+        $index++;
+      }
+      return $field_valid;
+    //} 
+    //return false;
+	}
+
+	public static function createAction() {
+
+	}
+
+	public static function deleteAction() {
+		// Delete record by id.
+    $url = "";
+    if (isset($_GET["fid"])) {
+      tom_update_record_by_id("a_form_forms", array("field_name_id" => ""), "field_name_id", $_GET["fid"]);
+      tom_update_record_by_id("a_form_forms", array("field_email_id" => ""), "field_email_id", $_GET["fid"]);
+      tom_update_record_by_id("a_form_forms", array("field_subject_id" => ""), "field_subject_id", $_GET["fid"]);
+      tom_delete_record_by_id("a_form_fields", "FID", $_GET["fid"]);
+      $url = get_option("siteurl")."/wp-admin/admin.php?page=a-forms/a-forms.php&action=edit&message=Record Deleted&id=".$_GET["form_id"]."";
+
+      tom_javascript_redirect_to($url, "<p>Please <a href='$url'>Click Next</a> to continue.</p>");
+    }
+    exit;
+	}
+}
+?>
---
+++ validations/a_form_validation.php
@@ -0,0 +1,70 @@
+<?php
+final class AFormValidation {
+
+
+  public static function is_valid_captcha($atts) {
+    $form = tom_get_row_by_id("a_form_forms", "*", "ID", $atts["id"]);
+    $form_name = "a_form_".str_replace(" ", "_", strtolower($form->form_name))."_";
+    $captcha_valid = false;
+    // Check to see if the user has clicked the Send button and check to see if the form is using a captcha.
+    if (isset($_POST["action"]) && $_POST["action"] == "Send" && isset($_POST[$form_name."captcha"]) && $form->include_captcha) {
+
+      // User clicked on Send button and the form has a captcha.
+      // Check the type of captcha.
+      if ($form->captcha_type == "0") {
+        // Form is using the Securimage Captcha.
+        $captcha_valid = tom_check_captcha($form_name."captcha");
+      } else {
+        // Form is using the Math Captcha.
+
+        // Check that the answer is first number plus second number.
+        $captcha_valid = 
+        (
+          (
+            ($_POST[aform_field_name($form, "captcha_first_number")]) 
+            + 
+            ($_POST[aform_field_name($form, "captcha_second_number")])
+          ) 
+          == ($_POST[aform_field_name($form, "captcha")])
+        );
+
+        // Check to see if captcha is valid.
+        if ($captcha_valid == false) {
+          // Captcha is invalid, so display error message.
+          $_SESSION["a_form_".str_replace(" ", "_", strtolower($form->form_name))."_captcha_error"] = "invalid captcha code, try again!";
+        }
+      }
+    }
+
+    return $captcha_valid;
+  }
+
+	public static function is_valid($atts) {
+    $validation_array = array();
+    $form = tom_get_row_by_id("a_form_forms", "*", "ID", $atts["id"]);
+    $sections = tom_get_results("a_form_sections", "*", "form_id='".$atts["id"]."'", array("section_order ASC"));
+
+    $form_name = "a_form_".str_replace(" ", "_", strtolower($form->form_name))."_";
+
+    $form_valid = true;
+    $section_index = 0;
+
+    if (isset($_POST["send_a_form_section"])) {
+      $section_index = ($_POST["send_a_form_section"]);
+    } else {
+      $section_index = 0;
+    }
+
+    // Get this section.
+    $section = $sections[$section_index];
+
+    // Add validation for this section only.
+    $fields = tom_get_results("a_form_fields", "*", "section_id='".$section->ID."'");
+    foreach ($fields as $field) {
+      $field_name = str_replace(" ", "_", strtolower($field->field_label));
+      $validation_array[$form_name.$field_name] = $field->validation;
+    }
+		return tom_validate_form($validation_array);
+	}
+}
+?>
